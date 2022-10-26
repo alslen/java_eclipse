73adff830c19e5fe9cba6b52f46195d9a084c129
admin <admin@DESKTOP-RI1RKA3.mshome.net>
2022-10-26 16:21:33 +0900

    ch13

---
+++ Ch13_1024/src/lambda/StringConCatImpl.java
@@ -0,0 +1,9 @@
+package lambda;
+
+public class StringConCatImpl implements StringConcat{
+
+	@Override
+	public void makeString(String s1, String s2) {
+		System.out.println( "일반 메서드를 호출 : "+s1 + "," + s2 );
+	}
+}
