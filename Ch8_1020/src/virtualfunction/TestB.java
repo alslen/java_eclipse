e41d31622dd032f80f0c60876ab138925e9005c2
admin <admin@DESKTOP-RI1RKA3.mshome.net>
2022-10-26 16:25:15 +0900

    ch08

---
+++ Ch8_1020/src/virtualfunction/TestB.java
@@ -0,0 +1,12 @@
+package virtualfunction;
+
+public class TestB extends TestA{
+	
+	
+//	void aaa(){
+//		System.out.println("콘설 출력을 3개로 수정.");
+//		System.out.println("aaa() 출력1");
+//		System.out.println("aaa() 출력2");
+//	}
+
+}
